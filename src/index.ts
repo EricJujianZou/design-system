b583402205a84008f86c4d8ac90ffba281042e05
EricJujianZou <e2zou@uwaterloo.ca>
2026-06-21 16:39:23 -0400

    adw auto: edit src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ export { Input } from "./components/input";
 export type { InputProps } from "./components/input";
 export { Card, CardHeader, CardContent, CardFooter } from "./components/card";
 export type { CardProps } from "./components/card";
+export { Text, textVariants } from "./components/text";
+export type { TextProps } from "./components/text";
 export { cn } from "./lib/utils";
